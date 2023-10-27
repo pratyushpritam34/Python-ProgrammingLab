58672a207c272cdcae0fcc8beb137de8128ab995
Pratyush Pritam <67601762+pratyushpritam34@users.noreply.github.com>
2023-10-27 10:34:11 +0530

    Update 2.Animal_sound.py

--- Object Oriented Programming using Python Assignment-2/2.Animal_sound.py
+++ Object Oriented Programming using Python Assignment-2/2.Animal_sound.py
@@ -17,12 +17,12 @@ class Cow(Animal):
 
 animal_type = input("Enter the type of animal (Sheep, Lion, Cat): ")
 
-if animal_type == "Sheep":
-    animal = Sheep()
-elif animal_type == "Lion":
-    animal = Lion()
+if animal_type == "Dog":
+    animal = Dog()
 elif animal_type == "Cat":
     animal = Cat()
+elif animal_type == "Cow":
+    animal = Cow()
 else:
     print("Animal type not recognized.")
     exit()
